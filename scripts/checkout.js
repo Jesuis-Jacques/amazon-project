e5484397410549c333658bf8c28a15b3358bb0e6
jacques <jamiercaryourday@gmail.com>
2024-08-17 21:21:03 +0100

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,13 +2,59 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
 
 //import '../data/backend-practice.js';
 //import '../data/car.js';
 
-loadProducts(() => {
+
+//Promise.all receives a list of promises to run before running any other in .then()
+
+Promise.all([
+  new Promise(() => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-})
+});
+
+/*
+new Promise(() => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+
+/* this next lines of code are Callbacks, does same as above which is a Promise
+
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+});
+*/
